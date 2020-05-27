3e0b07c9f448732884f970aaabaaea664f9972b3
Michael Wahler <michael.s.wahler@gmail.com>
2020-05-27 22:48:09 +0200

    Trained neural network will be saved and re-used in subsequent executions

--- src/demo.r
+++ src/demo.r
@@ -5,6 +5,10 @@
 # __version__ = "0.1"
 # __status__ = "Prototype"
 
+library(neuralnet)
+library(readr)
+
+stored_model_filename = "../generated/model.rds"
 
 # This function runs the data in csv_filename through the neural net nn
 run_net <- function(csv_filename, name)
@@ -30,12 +34,28 @@ cat (sprintf("- creates training and test data from .wav files and stores them a
 cat (sprintf("- loads the generated training data from .csv and creates a neural network\n"))
 cat (sprintf("- loads some test data and evaluates it on the neural network\n"))
 
-# generate ../generated/training_data.csv
-source ("create_training_data.r")
+
 # generate ../generated/test_data_*.csv
 source ("create_test_data.r")
-# trains a neural network referenced by variable 'nn'
-source ("net.r")
+
+
+#TODO
+# test if ../generated/model.rds exist
+# if it does, 
+# nn <- readRDS("../generated/model.rds")
+# and skip the "source" lines create_training_data and net.r
+
+if (file.exists(stored_model_filename)) 
+{
+    nn <- readRDS("../generated/model.rds")
+    cat (sprintf("Found stored model in %s, loading it instead of training a new model.\n", stored_model_filename))
+} else 
+{
+    # generate ../generated/training_data.csv
+    source ("create_training_data.r")
+    # trains a neural network referenced by variable 'nn'
+    source ("net.r")
+}
 
 # run the neural network with the test data
 
@@ -46,3 +66,11 @@ run_net ("../generated/test_data_barack.csv", "Barack")
 run_net ("../generated/test_data_donald.csv", "Donald")
 
 run_net ("../generated/test_data_george.csv", "George")
+
+
+# save neural network model
+if (! file.exists(stored_model_filename)) {
+    saveRDS (nn, file=stored_model_filename) 
+    cat (sprintf("\n\nStored neural network model in %s, it will be reused on the next run.\n",
+    stored_model_filename))
+}
